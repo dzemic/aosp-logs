dd3b3ca694c547a71c23307e1d2757df40072574
dzemic <anes.dzemic@cariad.technology>
2025-05-26 13:22:56 +0000

    works

--- main.py
+++ main.py
@@ -8,7 +8,38 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Cloud Run (Python)!"
+    project_id = os.environ.get("GCP_PROJECT_ID", "your-gcp-project-id")
+    location = "us-central1"
+
+    model_name = "gemini-2.0-flash-001"
+
+
+    vertexai.init(project=project_id, location=location)
+    model = GenerativeModel(model_name)
+
+
+    generation_config = GenerationConfig(
+            temperature=0.2, # Lower temperature for more factual, less creative responses
+            # max_output_tokens=2048, # Adjust as needed
+        )
+
+
+
+    prompt = f"""
+        You are an expert debugging assistant
+    """
+
+
+    response = model.generate_content(
+           [Part.from_text(prompt)],
+           generation_config=generation_config,
+           # safety_settings=... # Add safety settings if needed
+        )
+
+
+    #return f"Hello from Cloud Run! GCP_PROJECT_ID={project_id}\n"
+    return response.text 
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
